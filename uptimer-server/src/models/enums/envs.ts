39cb5f7ea39768de49248672ac4567b02a6f8cc2
Gabriel Dantas <gabrieldantaslp@gmail.com>
2025-02-06 16:57:05 -0300

    Apollo server - part 1

---
+++ uptimer-server/src/models/enums/envs.ts
@@ -0,0 +1,5 @@
+export enum Envs {
+  PROD = "production",
+  DEV = "development",
+  STAG = "staging",
+}
--- uptimer-server/src/server/server.ts
+++ uptimer-server/src/server/server.ts
@@ -1,18 +1,59 @@
 import http from "http";
 
 import { Express, NextFunction, Request, Response } from "express";
-import { PORT } from "./config";
+import { NODE_ENV, PORT } from "./config";
+import { ApolloServer } from "@apollo/server";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { ApolloServerPluginLandingPageDisabled } from "@apollo/server/plugin/disabled";
+import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default";
+import { Envs } from "@app/models/enums/envs";
+
+// TESTS
+const typeDefs = `#graphql
+  type User {
+    username: String
+  }
+
+  type Query {
+    user: User
+  }
+`;
+const resolvers = {
+  Query: {
+    user() {
+      return { username: "Gabriel" };
+    },
+  },
+};
 
 export default class MonitorServer {
   private app: Express;
   private httpServer: http.Server;
+  private server: ApolloServer;
 
   constructor(app: Express) {
     this.app = app;
     this.httpServer = new http.Server(app);
+    const schema = makeExecutableSchema({ typeDefs, resolvers });
+    this.server = new ApolloServer({
+      schema,
+      introspection: NODE_ENV !== Envs.PROD,
+      plugins: [
+        ApolloServerPluginDrainHttpServer({ httpServer: this.httpServer }),
+        NODE_ENV === Envs.PROD
+          ? ApolloServerPluginLandingPageDisabled()
+          : ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+      ],
+    });
   }
 
   async start(): Promise<void> {
+    /**
+     * Note that you must call the start() method on the ApolloServer
+     * instance before passing the instance to expressMiddleware
+     */
+    await this.server.start(); // Only if using expressMiddleWare (not startStandaloneServer)
     this.standartMiddleware(this.app);
     this.startServer();
   }
