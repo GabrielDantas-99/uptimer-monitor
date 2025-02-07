0e6b8d1d3b2aa1585cf238645fe8b8728a7d8662
Gabriel Dantas <gabrieldantaslp@gmail.com>
2025-02-07 20:27:58 -0300

    Updating home page layout

---
+++ uptimer-client/src/app/_components/Footer.tsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <div className="w-full bg-neutral-200/30">
+      <div className=" flex items-center justify-between  px-8 md:px-12 py-6 w-full max-w-6xl mx-auto">
+        <small>
+          © 2025 Phantom Tecnologia & Inovação - Todos os direitos reservados.
+        </small>
+        <small>
+          Desenvolvido por <a className="underline" href="https://github.com/GabrielDantas-99">Gabriel Dantas</a>
+        </small>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ uptimer-client/src/app/consts/monitors.ts
@@ -0,0 +1,27 @@
+export const MONITORS = [
+  {
+    title: 'Web Service Availability Check (HTTP/HTTPS)',
+    description:
+      'Analyzes the availability and response time of web applications through HTTP/HTTPS requests.',
+  },
+  {
+    title: 'Network Connectivity Test (TCP)',
+    description:
+      'Ensures servers are accessible by verifying TCP connections on specific ports.',
+  },
+  {
+    title: 'NoSQL Database Health Check (MongoDB)',
+    description:
+      'Evaluates the availability and connectivity of MongoDB database instances.',
+  },
+  {
+    title: 'In-Memory Storage Performance (Redis)',
+    description:
+      'Monitors the stability and response time of the Redis in-memory database.',
+  },
+  {
+    title: 'Security Certificate Validation (SSL/TLS)',
+    description:
+      'Checks the validity and expiration of SSL/TLS certificates to ensure secure connections.',
+  },
+];
