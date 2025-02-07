92c6d385cda5b0775b64354d26974b00d71a03b3
Gabriel Dantas <gabrieldantaslp@gmail.com>
2025-02-07 18:52:43 -0300

    Add button and input components

---
+++ uptimer-client/src/app/_components/TextInput.tsx
@@ -0,0 +1,31 @@
+import { ITextInputProps } from '@/interfaces/input.interface';
+import { FC, ReactElement } from 'react';
+import { Input } from "@/components/ui/input"
+
+const TextInput: FC<ITextInputProps> = (props): ReactElement => {
+  return (
+    <Input
+      id={props.id}
+      name={props.name}
+      type={props.type}
+      value={props.value}
+      readOnly={props.readOnly}
+      checked={props.checked}
+      className={props.className}
+      maxLength={props.maxLength}
+      style={props.style}
+      placeholder={props.placeholder}
+      min={props.min}
+      max={props.max}
+      onChange={props.onChange}
+      onClick={props.onClick}
+      onFocus={props.onFocus}
+      onBlur={props.onBlur}
+      onKeyUp={props.onKeyUp}
+      onKeyDown={props.onKeyDown}
+      autoComplete="false"
+    />
+  )
+}
+
+export default TextInput;
