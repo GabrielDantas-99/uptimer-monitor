571bdf5f30986f29fa209fbbfb7994e694516faf
Gabriel Dantas <gabrieldantaslp@gmail.com>
2025-02-07 13:20:16 -0300

    Setup websocket connection

--- uptimer-client/src/queries/apolloClient.ts
+++ uptimer-client/src/queries/apolloClient.ts
@@ -4,21 +4,51 @@ import {
   createHttpLink,
   InMemoryCache,
   NormalizedCacheObject,
+  split,
 } from '@apollo/client';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { Kind, OperationTypeNode } from 'graphql';
+import { createClient } from 'graphql-ws';
 
 const httpUrl: string = 'http://localhost:5000/graphql';
+const wsUrl: string = 'ws://localhost:5000/graphql';
 
 const httpLink: ApolloLink = createHttpLink({
   uri: httpUrl,
   credentials: 'include',
 });
 
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: wsUrl,
+    retryAttempts: 20,
+    shouldRetry: () => true,
+    on: {
+      closed: () => {
+        console.log('Client closed.');
+      },
+      connected: () => {
+        console.log('Client connected.');
+      },
+    },
+  })
+);
+
 const cache: InMemoryCache = new InMemoryCache();
 
 const apolloClient: ApolloClient<NormalizedCacheObject> = new ApolloClient({
-  link: httpLink,
+  link: split(isSubscription, wsLink, httpLink),
   cache,
   connectToDevTools: true, // TODO: Set to false for production
 });
+
+function isSubscription({ query }: { query: any }): boolean {
+  const definition = getMainDefinition(query);
+  return (
+    definition.kind === Kind.OPERATION_DEFINITION &&
+    definition.operation === OperationTypeNode.SUBSCRIPTION
+  );
+}
 
 export { apolloClient };
