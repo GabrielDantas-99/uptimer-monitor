df06edcf0d2cda651693897b1a67b96d0c0f2093
Gabriel Dantas <gabrieldantaslp@gmail.com>
2025-02-10 18:05:41 -0300

    Toggle password input and updating form fields

--- web/src/app/(auth)/(pages)/create-account/useRegister.ts
+++ web/src/app/(auth)/(pages)/create-account/useRegister.ts
@@ -15,21 +15,28 @@ export const useRegister = (): IUserAuth => {
   const router: AppRouterInstance = useRouter();
   const [registerUser, { loading }] = useMutation(REGISTER_USER);
   const onRegisterSubmit = async (formData: FormData): Promise<void> => {
-    const resultSchema = registerSchema.safeParse(Object.fromEntries(formData));
-    if (!resultSchema.success) {
-      setValidationErrors({
-        username: resultSchema.error.format().username?._errors[0]!,
-        email: resultSchema.error.format().email?._errors[0]!,
-        password: resultSchema.error.format().password?._errors[0]!,
-      });
-    } else {
-      const result: FetchResult = await registerUser({
-        variables: { user: resultSchema.data },
-      });
-      if (result && result.data) {
-        // TODO: Add data to react context
-        router.push('/');
+    try {
+      const resultSchema = registerSchema.safeParse(
+        Object.fromEntries(formData)
+      );
+      if (!resultSchema.success) {
+        setValidationErrors({
+          username: resultSchema.error.format().username?._errors[0]!,
+          email: resultSchema.error.format().email?._errors[0]!,
+          password: resultSchema.error.format().password?._errors[0]!,
+        });
+      } else {
+        const result: FetchResult = await registerUser({
+          variables: { user: resultSchema.data },
+        });
+        if (result && result.data) {
+          // TODO: Add data to react context
+          router.push('/');
+        }
       }
+    } catch (error) {
+      // TODO: Add toast method
+      console.log(error);
     }
   };
   return {
--- web/src/app/_components/TextInput.tsx
+++ web/src/app/_components/TextInput.tsx
@@ -1,30 +1,45 @@
+'use client'
+
 import { ITextInputProps } from '@/interfaces/input.interface';
-import { FC, ReactElement } from 'react';
+import { FC, ReactElement, useState } from 'react';
 import { Input } from "@/components/ui/input"
+import { Eye, EyeClosed } from 'lucide-react';
 
 const TextInput: FC<ITextInputProps> = (props): ReactElement => {
+  const [showPassword, setShowPassword] = useState<boolean>(false)
   return (
-    <Input
-      id={props.id}
-      name={props.name}
-      type={props.type}
-      value={props.value}
-      readOnly={props.readOnly}
-      checked={props.checked}
-      className={props.className}
-      maxLength={props.maxLength}
-      style={props.style}
-      placeholder={props.placeholder}
-      min={props.min}
-      max={props.max}
-      onChange={props.onChange}
-      onClick={props.onClick}
-      onFocus={props.onFocus}
-      onBlur={props.onBlur}
-      onKeyUp={props.onKeyUp}
-      onKeyDown={props.onKeyDown}
-      autoComplete="false"
-    />
+    <div className='relative'>
+      {props.type === 'password' && (
+        <div onClick={() => setShowPassword(!showPassword)} className="absolute right-0 flex h-full cursor-pointer items-center pr-3">
+          {showPassword ? (
+            <Eye />
+          ) : (
+            <EyeClosed />
+          )}
+        </div>
+      )}
+      <Input
+        id={props.id}
+        name={props.name}
+        type={props.type === 'password' ? showPassword ? 'text' : 'password' : props.type}
+        value={props.value}
+        readOnly={props.readOnly}
+        checked={props.checked}
+        className={props.className}
+        maxLength={props.maxLength}
+        style={props.style}
+        placeholder={props.placeholder}
+        min={props.min}
+        max={props.max}
+        onChange={props.onChange}
+        onClick={props.onClick}
+        onFocus={props.onFocus}
+        onBlur={props.onBlur}
+        onKeyUp={props.onKeyUp}
+        onKeyDown={props.onKeyDown}
+        autoComplete="false"
+      />
+    </div>
   )
 }
 
