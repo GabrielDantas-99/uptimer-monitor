6d35d4bf202f2bef5dd607115f2c7a2b4142ea7f
Gabriel Dantas <gabrieldantaslp@gmail.com>
2025-02-24 18:07:08 -0300

    Home buttons count method

--- web/src/app/dashboard/_components/HomeButtonGroup.tsx
+++ web/src/app/dashboard/_components/HomeButtonGroup.tsx
@@ -1,5 +1,6 @@
 import Button from "@/app/_components/Button";
 import { IMonitorDocument } from "@/interfaces/monitor.interface";
+import { filter } from "lodash";
 import { ArrowDown, ArrowUp, Play } from "lucide-react";
 
 interface IHomeButtonGroupProps {
@@ -7,11 +8,42 @@ interface IHomeButtonGroupProps {
 }
 
 const HomeButtonGroup = ({ monitors }: IHomeButtonGroupProps) => {
+  const count = (type: string): number => {
+    let sum = 0;
+    if (type === 'active') {
+      sum = filter(monitors, (monitor: IMonitorDocument) => monitor.active && monitor.status === 0).length;
+    }
+    if (type === 'inactive') {
+      sum = filter(monitors, (monitor: IMonitorDocument) => !monitor.active).length;
+    }
+    if (type === 'error') {
+      sum = filter(monitors, (monitor: IMonitorDocument) => monitor.active && monitor.status === 1).length;
+    }
+    return sum;
+  }
   return (
     <div className="inline-flex gap-2" role="group">
-      <Button className="font-medium" variant="success" label={1} icon={<ArrowUp />} type="button" />
-      <Button className="font-medium" variant="destructive" label={1} icon={<ArrowDown />} type="button" />
-      <Button className="font-medium" variant="warn" label={1} icon={<Play />} type="button" />
+      <Button
+        className="font-medium"
+        variant="success"
+        label={count('active')}
+        icon={<ArrowUp />}
+        type="button"
+      />
+      <Button
+        className="font-medium"
+        variant="destructive"
+        label={count('error')}
+        icon={<ArrowDown />}
+        type="button"
+      />
+      <Button
+        className="font-medium"
+        variant="warn"
+        label={count('inactive')}
+        icon={<Play />}
+        type="button"
+      />
     </div>
   );
 }
