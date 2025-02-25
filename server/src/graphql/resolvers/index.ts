ec4ee76520ee25dcb96ffedc48aa0cddc2db3952
Gabriel Dantas <gabrieldantaslp@gmail.com>
2025-02-25 18:22:11 -0300

    GraphQL heartbeat schema and resolver

--- server/src/graphql/resolvers/index.ts
+++ server/src/graphql/resolvers/index.ts
@@ -1,5 +1,11 @@
+import { HeartbeatResolver } from "./heartbeats.resolver";
 import { MonitorResolver } from "./monitor.resolver";
 import { NotificationResolver } from "./notification.resolver";
 import { UserResolver } from "./user.resolver";
 
-export const resolvers = [UserResolver, NotificationResolver, MonitorResolver];
+export const resolvers = [
+  UserResolver,
+  NotificationResolver,
+  MonitorResolver,
+  HeartbeatResolver,
+];
--- server/src/graphql/schema/index.ts
+++ server/src/graphql/schema/index.ts
@@ -2,9 +2,11 @@ import { mergeTypeDefs } from "@graphql-tools/merge";
 import { userSchema } from "./user.schema";
 import { notificationSchema } from "./notification.schema";
 import { monitorSchema } from "./monitor.schema";
+import { heartbeatSchema } from "./heartbeats.schema";
 
 export const mergedGQLSchema = mergeTypeDefs([
   userSchema,
   notificationSchema,
   monitorSchema,
+  heartbeatSchema,
 ]);
