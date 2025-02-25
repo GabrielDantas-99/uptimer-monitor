3461f1a04d240033d9431beb5b7c0be3258006b5
Gabriel Dantas <gabrieldantaslp@gmail.com>
2025-02-25 15:25:27 -0300

    Create and get http heartbeats

---
+++ server/src/services/http.service.ts
@@ -0,0 +1,42 @@
+import { IHeartbeat } from "@app/interfaces/heartbeat.interface";
+import { HttpModel } from "@app/models/http.model";
+import dayjs from "dayjs";
+import { Model, Op } from "sequelize";
+
+export const createHttpHeartBeat = async (
+  data: IHeartbeat
+): Promise<IHeartbeat> => {
+  try {
+    const result: Model = await HttpModel.create(data);
+    return result.dataValues;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const getHttpHeartBeatsByDuration = async (
+  monitorId: number,
+  duration = 24
+): Promise<IHeartbeat[]> => {
+  try {
+    const dateTime: Date = dayjs.utc().toDate();
+    dateTime.setHours(dateTime.getHours() - duration);
+    const heartbeats: IHeartbeat[] = (await HttpModel.findAll({
+      raw: true,
+      where: {
+        [Op.and]: [
+          { monitorId },
+          {
+            timestamp: {
+              [Op.gte]: dateTime,
+            },
+          },
+        ],
+      },
+      order: [["timestamp", "DESC"]],
+    })) as unknown as IHeartbeat[];
+    return heartbeats;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
